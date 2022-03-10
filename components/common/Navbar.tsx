fbede9d5fcd8f7a57b2660b729704a0c3eb20741
Francisco Castro <francisco.castro@sooper.com.br>
2022-03-09 22:57:07 -0300

    Make navbar fixed and fullwidth

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -13,72 +13,77 @@ const Navbar: FC = () => {
   const { pathname } = useRouter();
 
   return (
-    <nav className="navbar navbar-expand navbar-light bg-light justify-content-between">
-      {/* <div className="container"> */}
-        {/* <div className="row justify-content-between"> */}
-          {/* Brand */}
-          <div className="col">
+    <nav
+      className="navbar navbar-expand navbar-light bg-light justify-content-between"
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        right: 0,
+        zIndex: 1,
+      }}
+    >
+      {/* Brand */}
+      <div className="col">
+        <Link href="/" passHref>
+          <a className="navbar-brand fw-bold">Blog</a>
+        </Link>
+      </div>
+
+      {/* Search */}
+      <div className="col d-none d-sm-flex">
+        <form className={styles.searchForm + " justify-content-center"}>
+          <button type="submit">
+            <AiOutlineSearch />
+          </button>
+          <input className="me-2" type="search" placeholder="Search..." />
+        </form>
+      </div>
+
+      {/* Navigation */}
+      <div className="col d-none d-md-flex justify-content-end">
+        <ul className="navbar-nav mb-2 mb-lg-0 justify-content-between">
+          <li className="nav-item mx-2">
             <Link href="/" passHref>
-              <a className="navbar-brand fw-bold">Blog</a>
+              <a className={"nav-link " + (pathname === "/" && "active")}>
+                Home
+              </a>
             </Link>
-          </div>
-
-          {/* Search */}
-          <div className="col d-none d-sm-flex">
-            <form className={styles.searchForm + " justify-content-center"}>
-              <button type="submit">
-                <AiOutlineSearch />
-              </button>
-              <input className="me-2" type="search" placeholder="Search..." />
-            </form>
-          </div>
-
-          {/* Navigation */}
-          <div className="col d-none d-md-flex justify-content-end">
-            <ul className="navbar-nav mb-2 mb-lg-0 justify-content-between">
-              <li className="nav-item mx-2">
-                <Link href="/" passHref>
-                  <a className={"nav-link " + (pathname === "/" && "active")}>
-                    Home
-                  </a>
-                </Link>
-              </li>
-              <li className="nav-item mx-2">
-                <Link href="/posts" passHref>
-                  <a
-                    className={
-                      "nav-link " + (pathname === "/posts" && "active")
-                    }
-                  >
-                    Posts
-                  </a>
-                </Link>
-              </li>
-              <li className="nav-item mx-2">
-                <Link href="https://instagram.com/mcpeblocker" passHref>
-                  <a className="nav-link" target="_blank">
-                    <AiFillInstagram />
-                  </a>
-                </Link>
-              </li>
-              <li className="nav-item mx-2">
-                <Link href="https://twitter.com/mcpeblocker" passHref>
-                  <a className="nav-link" target="_blank">
-                    <AiOutlineTwitter />
-                  </a>
-                </Link>
-              </li>
-              <li className="nav-item mx-2">
-                <Link href="https://linked.in/mcpeblocker" passHref>
-                  <a className="nav-link" target="_blank">
-                    <AiFillLinkedin />
-                  </a>
-                </Link>
-              </li>
-            </ul>
-          </div>
-        {/* </div> */}
-      {/* </div> */}
+          </li>
+          <li className="nav-item mx-2">
+            <Link href="/posts" passHref>
+              <a
+                className={
+                  "nav-link " + (pathname === "/posts" && "active")
+                }
+              >
+                Posts
+              </a>
+            </Link>
+          </li>
+          <li className="nav-item mx-2">
+            <Link href="https://instagram.com/mcpeblocker" passHref>
+              <a className="nav-link" target="_blank">
+                <AiFillInstagram />
+              </a>
+            </Link>
+          </li>
+          <li className="nav-item mx-2">
+            <Link href="https://twitter.com/mcpeblocker" passHref>
+              <a className="nav-link" target="_blank">
+                <AiOutlineTwitter />
+              </a>
+            </Link>
+          </li>
+          <li className="nav-item mx-2">
+            <Link href="https://linked.in/mcpeblocker" passHref>
+              <a className="nav-link" target="_blank">
+                <AiFillLinkedin />
+              </a>
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
