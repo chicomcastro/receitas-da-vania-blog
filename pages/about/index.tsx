2a73cde6cdf54d910b0da49f91fcaf26ead71920
Francisco Castro <francisco.castro@sooper.com.br>
2022-03-15 19:25:01 -0300

    Add about page

---
+++ pages/about/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+import Layout from "../../components/Layout";
+
+const About: NextPage = () => {
+  return (
+    <Layout>
+      Sobre mim
+    </Layout>
+  );
+}
+
+export default About;
