c66d41afaf7ce014ae314ac271449ab6608dc5cd
Francisco Castro <francisco.castro@sooper.com.br>
2022-03-15 21:57:54 -0300

    Add footer

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -12,8 +12,19 @@ const Layout: FC<Props> = ({ children, seo }) => {
     <div className="container">
       <SEO {...seo} />
       <Navbar />
-      <div style={{ marginBottom: 80 }} />
+      <div className="d-none d-md-flex" style={{ marginBottom: 80 }} />
+      <div className="d-flex d-md-none" style={{ marginBottom: 136 }} />
       {children}
+      <div
+        style={{
+          fontSize: 12,
+          display: "flex",
+          justifyContent: "center",
+          marginBottom: 8,
+        }}
+      >
+        © 2022 Receitas da Vânia, todos os direitos reservados
+      </div>
     </div>
   );
 };
