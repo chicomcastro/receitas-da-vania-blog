878c446e7e67e667725370dad4208a054209a451
Francisco Castro <francisco.castro@sooper.com.br>
2022-03-15 21:58:59 -0300

    Type home correctly

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,15 @@
 import type { NextPage } from "next";
-import { getPosts } from "../clients/GoogleSheets";
+import { getPosts, Post } from "../clients/GoogleSheets";
 import Header from "../components/home/Header";
 import Recommendations from "../components/home/Recommendations";
 import Layout from "../components/Layout";
-import styles from "../styles/Home.module.css";
 
-const Home: NextPage<{ recommendedPosts: any[] }> = ({ recommendedPosts = [] }) => {
+const Home: NextPage<{ recommendedPosts: Post[] }> = ({ recommendedPosts = [] }) => {
   return (
     <Layout>
       <Header />
       <Recommendations posts={recommendedPosts} />
+      <div style={{ marginBottom: 40 }} />
     </Layout>
   );
 };
@@ -23,7 +23,7 @@ export async function getStaticProps(context: any) {
     // Next.js will attempt to re-generate the page:
     // - When a request comes in
     // - At most once every second
-    revalidate: 1, // In seconds
+    revalidate: 60, // In seconds
   };
 }
 
